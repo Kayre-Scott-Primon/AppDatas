261d8e59cb68e6c1e86d7585af8b83eb1c7285fe
KayreScottPrimon <kayre@sensix.com.br>
2022-03-24 18:00:18 -0300

    starting settings bluetooth tools

--- src/screens/bluetooth/index.js
+++ src/screens/bluetooth/index.js
@@ -1,14 +1,46 @@
 import React, { Component, useEffect, useState } from 'react'
-import { FlatList, Text, View } from 'react-native'
+import { FlatList, Text, View, NativeModules, NativeAppEventEmitter } from 'react-native'
 import { Icon } from "react-native-elements";
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 import CardMenu from '../../components/cardMenu';
 import Header from '../../components/header'
 import { menuList } from './data';
 import styles from './styles'
-import { BleManager } from 'react-native-ble-plx';
+import BleManager from 'react-native-ble-manager';
+
 
 function Bluetooth(){
+
+    const [state, setState] = useState(false)
+
+    useEffect(() => {
+        console.log('bluetooth scanner mounted');
+
+        NativeAppEventEmitter.addListener('BleManagerDiscoverPeripheral',(data) => 
+        {
+            let device = 'device found: ' + data.name + '(' + data.id + ')'; 
+
+            if(this.devices.indexOf(device) == -1) {
+                devices.push(device);
+            }
+
+            let newState = this.state;
+            newState.dataSource = newState.dataSource.cloneWithRows(this.devices);
+            setState(newState);
+        });
+
+        BleManager.start({showAlert: false})
+            .then(() => {
+                // Success code 
+                console.log('Module initialized');
+        });
+    })
+
+    function startScanning() {
+        console.log('start scanning');
+        BleManager.scan([], 120);
+    }
+    startScanning()
 
     return(
         <View style={styles.container}>
